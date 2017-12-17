ce59d2455c8346b7d1eddadee170286bb83d0f02
Roustique <rustam-crocodile@mail.ru>
2017-12-17 13:28:30 +0300

    Beta 1.0.13

--- TASK.f90
+++ TASK.f90
@@ -10,7 +10,7 @@ integer(4) mpiErr, mpiSize, mpiRank
 integer(4), dimension(4) :: t
 real(8) pr, S, maxS, tmaxS
 
-X=0; tx1=1; tx2=1; ty1=1; ty2=1; maxS=A(1,1)
+X=0; t(1)=1; t(2)=1; t(3)=1; t(4)=1; maxS=A(1,1)
 m=size(A(:,1))
 n=size(A(1,:))
 
@@ -55,6 +55,7 @@ do i=mpiRank,n,mpiSize
 enddo
 
 if (mpiRank == 0) then
+   write(*,*)t(2)
    maxS=tmaxS
    x1=t(1)
    x2=t(2)
@@ -62,15 +63,20 @@ if (mpiRank == 0) then
    y2=t(4)
    do k=1,(mpiSize-1)
     call mpi_recv(t, 4, MPI_INTEGER4, MPI_ANY_SOURCE, k, MPI_COMM_WORLD, status, mpiErr)
-    call mpi_recv(t, 4, MPI_INTEGER4, MPI_ANY_SOURCE, (k+4), MPI_COMM_WORLD, status, mpiErr)
-    if (tmaxS>maxS) then
+    call mpi_recv(tmaxS, 1, MPI_REAL8, MPI_ANY_SOURCE, (k+4), MPI_COMM_WORLD, status, mpiErr)
+    write(*,*)k, tmaxS, maxS, t
+    if ((tmaxS>maxS).AND.(t(2)/=0)) then
+     write(*,*)t(3), "ne ravno nulyu"
      maxS=tmaxS
      x1=t(1)
      x2=t(2)
      y1=t(3)
-     y2=t(4)    
+     y2=t(4)  
+     !write(*,*)x1  
     endif
+    !write(*,*)maxS
    enddo
+   !write(*,*)x1, x2, y1, y2
   else
    call mpi_send(t, 4, MPI_INTEGER4, 0, mpiRank, MPI_COMM_WORLD, mpiErr)
    call mpi_send(tmaxS, 1, MPI_REAL8, 0, (mpiRank+4), MPI_COMM_WORLD, mpiErr)
