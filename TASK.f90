2916d4cfd11a63c17958fe8257e9277688bca157
Roustique <rustam-crocodile@mail.ru>
2017-12-17 13:45:53 +0300

    Beta 1.1.2

--- TASK.f90
+++ TASK.f90
@@ -5,12 +5,11 @@ include "mpif.h"
 real(8), intent(in), dimension(:,:) :: A
 real(8), dimension(size(A(:,1))) :: B
 integer(4), intent(out) :: x1, y1, x2, y2
-integer(4) n, m, i, j, k, minn, maxx, x, y
+integer(4) n, m, i, j, k, minn, maxx, x, y, tx1, tx2, ty1, ty2
 integer(4) mpiErr, mpiSize, mpiRank
-integer(4), dimension(4) :: t
 real(8) pr, S, maxS, tmaxS
 
-X=0; t(1)=1; t(2)=1; t(3)=1; t(4)=1; maxS=A(1,1)
+X=0; tx1=1; tx2=1; ty1=1; ty2=1; maxS=A(1,1)
 m=size(A(:,1))
 n=size(A(1,:))
 
@@ -46,40 +45,40 @@ do i=mpiRank,n,mpiSize
   enddo
   if (S>tmaxS) then
    tmaxS=S
-   t(1)=i
-   t(2)=j
-   t(3)=minn
-   t(4)=maxx
+   tx1=i
+   tx2=j
+   ty1=minn
+   ty2=maxx
   endif
  enddo
 enddo
+!write(*,*)tmaxS, mpiRank
 
 if (mpiRank == 0) then
-   write(*,*)t(2)
-   maxS=tmaxS
-   x1=t(1)
-   x2=t(2)
-   y1=t(3)
-   y2=t(4)
    do k=1,(mpiSize-1)
-    call mpi_recv(t, 4, MPI_INTEGER4, MPI_ANY_SOURCE, k, MPI_COMM_WORLD, status, mpiErr)
-    call mpi_recv(tmaxS, 1, MPI_REAL8, MPI_ANY_SOURCE, (k+4), MPI_COMM_WORLD, status, mpiErr)
-    write(*,*)k, tmaxS, maxS, t
-    if ((tmaxS>maxS).AND.(t(2)/=0)) then
-     write(*,*)t(3), "ne ravno nulyu"
+    call mpi_recv(tx1, 1, MPI_INTEGER4, MPI_ANY_SOURCE, 5*(k-1), MPI_COMM_WORLD, status, mpiErr)
+    call mpi_recv(tx2, 1, MPI_INTEGER4, MPI_ANY_SOURCE, 5*(k-1)+1, MPI_COMM_WORLD, status, mpiErr)
+    call mpi_recv(ty1, 1, MPI_INTEGER4, MPI_ANY_SOURCE, 5*(k-1)+2, MPI_COMM_WORLD, status, mpiErr)
+    call mpi_recv(ty2, 1, MPI_INTEGER4, MPI_ANY_SOURCE, 5*(k-1)+3, MPI_COMM_WORLD, status, mpiErr)
+    call mpi_recv(tmaxS, 1, MPI_REAL8, MPI_ANY_SOURCE, 5*(k-1)+4, MPI_COMM_WORLD, status, mpiErr)
+    !write(*,*)k, tmaxS, maxS, tx1, tx2, ty1, ty2
+    if ((tmaxS>maxS).AND.(tx2/=0)) then
      maxS=tmaxS
-     x1=t(1)
-     x2=t(2)
-     y1=t(3)
-     y2=t(4)  
+     x1=tx1
+     x2=tx2
+     y1=ty1
+     y2=ty2  
      !write(*,*)x1  
     endif
     !write(*,*)maxS
    enddo
    !write(*,*)x1, x2, y1, y2
   else
-   call mpi_send(t, 4, MPI_INTEGER4, 0, mpiRank, MPI_COMM_WORLD, mpiErr)
-   call mpi_send(tmaxS, 1, MPI_REAL8, 0, (mpiRank+4), MPI_COMM_WORLD, mpiErr)
+   call mpi_send(tx1, 1, MPI_INTEGER4, 0, 5*(mpiRank-1), MPI_COMM_WORLD, mpiErr)
+   call mpi_send(tx2, 1, MPI_INTEGER4, 0, 5*(mpiRank-1)+1, MPI_COMM_WORLD, mpiErr)
+   call mpi_send(ty1, 1, MPI_INTEGER4, 0, 5*(mpiRank-1)+2, MPI_COMM_WORLD, mpiErr)
+   call mpi_send(ty2, 1, MPI_INTEGER4, 0, 5*(mpiRank-1)+3, MPI_COMM_WORLD, mpiErr)
+   call mpi_send(tmaxS, 1, MPI_REAL8, 0, 5*(mpiRank-1)+4, MPI_COMM_WORLD, mpiErr)
 endif
 
 call mpi_bcast(x1, 4, MPI_INTEGER4, 0, MPI_COMM_WORLD, mpiErr)
